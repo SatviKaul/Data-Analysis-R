d26699c24ffc3e2eef3e4af50bb6ff386e5d6f9e
SatviKaul <satvik.1799@gmail.com>
2019-06-06 14:48:02 +0530

    Create Variables.r

---
+++ R Basics/Variables.r
@@ -0,0 +1,12 @@
+# Single line comment
+#Variables start with lower case
+variable <- 100
+print(variable)
+bank <- 1000
+print(bank)
+bank.account <- 10000
+bankAccount <- 10
+#Using underscore is bad practise
+print(bank.account+bankAccount)
+bank.account <- bank.account+bankAccount
+print(bank.account)
