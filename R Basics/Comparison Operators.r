fe16f0190c66caa90c19862f555e5253c0454172
SatviKaul <satvik.1799@gmail.com>
2019-06-06 14:48:17 +0530

    Create Comparison Operators.r

---
+++ R Basics/Comparison Operators.r
@@ -0,0 +1,16 @@
+#Basic
+5 > 6#Greater than
+5 < 6#Less than
+5 <= 6#Less than equal to
+5 >= 6#Greater than equal to
+5 == 5#Equality Operator
+5 != 2#Not equal to
+#Spacing plays a major role
+v <- 1#Assigns 1 to v
+v < -1#Compares value of v with -1
+#Vector Comparisons
+v <- c(1,2,3,4,5)
+v < 2#Element by element comparison
+v == 3#Returns vector of logical values
+v2 <- c(2,3,4,5,6)
+v1 < v2#Vector to vector comparison
