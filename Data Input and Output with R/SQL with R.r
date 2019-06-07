757e105a991f182f2c2ce63b1f5ce31d89396021
SatviKaul <satvik.1799@gmail.com>
2019-06-07 12:05:41 +0530

    Create SQL with R.r

---
+++ Data Input and Output with R/SQL with R.r
@@ -0,0 +1,7 @@
+#install.packages("RODBC")
+#install.packages("RMySQL")
+#install.packages("ROracle")
+#Install whatever system you're comfortable in
+#r-bloggers is an amazing site for basic syntax and wonderful examples with step by step tutorials
+#Most important part is the Connection with R code snippet
+#Best method is to do a little bit of independent research for learning SQL
