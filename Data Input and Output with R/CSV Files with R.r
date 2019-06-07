da0524e990edbc8a9b28562e1e6f32dbae413687
SatviKaul <satvik.1799@gmail.com>
2019-06-07 12:05:32 +0530

    Update CSV Files with R.r

--- Data Input and Output with R/CSV Files with R.r
+++ Data Input and Output with R/CSV Files with R.r
@@ -1,1 +1,10 @@
 write.csv(mtcars, file = 'my_example.csv')
+ex <- read.csv('my_example.csv')
+head(ex)
+class(ex)
+#All operations of data frames can be performed with csv files
+write.csv(ex, file = 'my_new_example.csv')
+
+#help can be found for any term using either
+#??term; or
+#help(term)
