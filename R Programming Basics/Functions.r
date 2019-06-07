e294a389a54e81942916ded9c03bf1f0c27f3bb1
SatviKaul <satvik.1799@gmail.com>
2019-06-07 12:06:00 +0530

    Create Functions.r

---
+++ R Programming Basics/Functions.r
@@ -0,0 +1,10 @@
+#Basic syntax for declaring a function
+
+#name_of_function <- function(input1,input2,input3=45){
+  #Execute code
+  #result<-input1+input2
+  #return(result), return allows us to save output in a variable
+#}
+
+#Calling function
+#function_name(), parenthesis necessary
